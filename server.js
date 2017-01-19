6dc279a1218142236f6c0261d380499e89aa1bd0
0blu3 <braelynn.luedtke@gmail.com>
2017-01-19 15:28:27 -0800

    express added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const requestProxy = require('express-request-proxy')
 const PORT = process.env.PORT || 4000
 
 app.use(express.static('./public'))
