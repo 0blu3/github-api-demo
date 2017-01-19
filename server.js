3b2bb5533539976936beed11796a75599cd693a6
0blu3 <braelynn.luedtke@gmail.com>
2017-01-19 15:42:23 -0800

    ?

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
-const requestProxy = require('express-request-proxy')
+const requestProxy = require('express-request-proxy') //request expres proxy
 const PORT = process.env.PORT || 4000
 
-app.use(express.static('./public'))
+app.use(express.static('./public')) //do this
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
+
+app.get('/github/*', proxyGithub); //do this
+
+function proxyGithub(req, res) => {
+  console.log('Routing a Github request for', req.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${req.params[0]}`
+    headers:{ Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(req, res);
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
